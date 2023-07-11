0bf9672f690147226340a0669db5edcd29730619
Bianca <bianca.burlacu1410@gmail.com>
2023-07-11 15:05:11 +0300

    Step definitions refactoring.

--- src/test/java/steps/UserStepDefinitions.java
+++ src/test/java/steps/UserStepDefinitions.java
@@ -2,36 +2,32 @@ package steps;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.apache.http.HttpStatus;
+import support.api.UserApi;
 import support.domain.User;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserStepDefinitions {
-    private User user;
-    private static final String CREATE_USER_ENDPOINT = "/v3/user";
-    private static final String USER_ENDPOINT = "/v3/user/{name}";
+    private User expectedUser;
+    private UserApi userApi;
+
+    public UserStepDefinitions(){
+        userApi = new UserApi();
+    }
 
     @When("I create a user")
     public void iCreateAUser() {
-        user = User.builder().build();
-        given().
-                body(user).
-                when().
-                post(CREATE_USER_ENDPOINT).
-                then().
-                statusCode(HttpStatus.SC_OK);
+        expectedUser = User.builder().build();
+        userApi.createUser(expectedUser);
 
     }
 
     @Then("the created user was stored")
     public void theCreatedUserWasStored() {
-        given().pathParam("name", user.getUsername()).
-                when().
-                get(USER_ENDPOINT).
-                then()
-                .statusCode(HttpStatus.SC_OK)
-                .body("username", is(user.getUsername()));
+        String actualUsername = userApi.getUsername(expectedUser);
+        assertThat(actualUsername,is(expectedUser.getUsername()));
     }
+
+
 }
