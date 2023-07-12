0fb9652d38b11725fb7763a796d6598b1c457efb
Bianca <bianca.burlacu1410@gmail.com>
2023-07-12 20:34:29 +0300

    Update CucumberRunner Adding GitHub actions job for pull requests

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         tags = "not @wip and not @quarantine",
-        plugin ={"pretty", "html:target/reports/feature.html"},
+        plugin ={"pretty", "html:target/reports/report.html"},
         features = {"src/test/resources/features"}
 )
 public class CucumberRunner {
